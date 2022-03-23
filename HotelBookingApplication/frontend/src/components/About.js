efeb321dc9feaba27e0ea217325a88dba065e709
sharansantosh97 <sharan.santosh.ss@gmail.com>
2022-03-23 13:48:14 -0700

    added base basic pages of UI

---
+++ HotelBookingApplication/frontend/src/components/About.js
@@ -0,0 +1,428 @@
+import React from 'react'
+
+const About = () => {
+  return <>
+  
+<section class="w3l-breadcrumb">
+    <div class="breadcrum-bg py-sm-5 py-4">
+        <div class="container py-lg-3">
+
+            <h2>About Us</h2>
+            <p><a href="index.html">Home</a> &nbsp; / &nbsp; About</p>
+
+        </div>
+    </div>
+</section>
+<div class="w3l-about-us best-rooms py-5">
+    <div class="container py-lg-5 py-sm-4">
+        <div class="maghny-gd-1">
+            <div class="row about-text">
+                <div class="col-lg-6">
+                    <h3 class="title-big">Best luxury Hotel in London since 2000. Our hotel has been present for over 20
+                        years.</h3>
+                </div>
+                {/* <div class="col-lg-6">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit tempore sapiente, distinctio
+                        perferendis voluptas consequatur quaerat incidunt. Similique, officia! Dolorum fugiat et aliquam
+                        necessitatibus quas reiciendis, totam voluptatibus deleniti tempore doloribus dolor modi, ab
+                        facilis quam officiis ipsam doloremque.</p>
+                    <p class="mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus veritatis incidunt iste totam,
+                        consequuntur minus assumenda commodi sint asperiores. Voluptatibus incidunt alias.</p>
+                </div> */}
+            </div>
+            <div class="row mt-5">
+                <div class="maghny-gd-1 col-lg-4 col-md-6">
+                    <div class="maghny-grid">
+                        <figure class="effect-lily border-radius">
+                            <img class="img-fluid" src="assets/images/about1.jpg"/>
+                            <figcaption class="w3set-hny">
+                                <div>
+                                    <h4>Top Restaurant <span> Hotels</span></h4>
+                                    <p>Breakfast and Dinner</p>
+                                </div>
+
+                            </figcaption>
+                        </figure>
+                    </div>
+                </div>
+                <div class="maghny-gd-1 col-lg-4 col-md-6 mt-md-0 mt-4">
+                    <div class="maghny-grid">
+                        <figure class="effect-lily border-radius">
+                            <img class="img-fluid" src="assets/images/about2.jpg"/>
+                            <figcaption class="w3set-hny">
+                                <div>
+                                    <h4>Spa and Wellness <span> Hotels</span></h4>
+                                    <p>Open Daily </p>
+                                </div>
+
+                            </figcaption>
+                        </figure>
+                    </div>
+                </div>
+                <div class="maghny-gd-1 col-lg-4 col-md-6 mt-lg-0 mt-4">
+                    <div class="maghny-grid">
+                        <figure class="effect-lily border-radius">
+                            <img class="img-fluid" src="assets/images/about3.jpg"/>
+                            <figcaption class="w3set-hny">
+                                <div>
+                                    <h4>Best Luxury Rooms <span> Hotels</span></h4>
+                                    <p>Cool view </p>
+                                </div>
+
+                            </figcaption>
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<section class="w3-about-2" id="features">
+    <div class="w3-services-ab py-5">
+        <div class="container py-lg-4 py-md-3">
+            <div class="title-section">
+
+                <div class="title-line">
+                </div>
+            </div>
+            <div class="row w3-services-grids">
+                <div class="col-lg-9 w3-services-right-grid pl-lg-5 mx-auto">
+                    <h4 class="mb-md-5 mb-4">Doing the right thing, at the right time</h4>
+                    <div class="fea-gd-vv row">
+                        <div class="col-md-6">
+                            <div class="float-lt feature-gd">
+                                <div class="icon"> <span class="fa fa-file-text-o" aria-hidden="true"></span></div>
+                                <div class="icon-info">
+                                    <h5>Welcome Drink</h5>
+                                    {/* <p> Lorem ipsum dolor sit amet, consectetur adipisicingelit, sed eiusmod tempor
+                                        incididunt </p> */}
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 mt-md-0 mt-4">
+                            <div class="float-mid feature-gd">
+                                <div class="icon"> <span class="fa fa-trophy" aria-hidden="true"></span></div>
+                                <div class="icon-info">
+                                    <h5>Bike Rental</h5>
+                                    {/* <p> Lorem ipsum dolor sit amet, consectetur adipisicingelit, sed eiusmod tempor
+                                        incididunt </p> */}
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 mt-4 pt-md-2">
+                            <div class="float-rt feature-gd">
+                                <div class="icon"> <span class="fa fa-clone" aria-hidden="true"></span></div>
+                                <div class="icon-info">
+                                    <h5>WIFI Areas</h5>
+                                    {/* <p> Lorem ipsum dolor sit amet, consectetur adipisicingelit, sed eiusmod tempor
+                                        incididunt </p> */}
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 mt-4 pt-md-2">
+                            <div class="float-lt feature-gd">
+                                <div class="icon"> <span class="fa fa-bullseye" aria-hidden="true"></span></div>
+                                <div class="icon-info">
+                                    <h5>Swimming Pool</h5>
+                                    {/* <p> Lorem ipsum dolor sit amet, consectetur adipisicingelit, sed eiusmod tempor
+                                        incididunt </p> */}
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+{/* 
+
+
+
+
+
+
+<section class="team py-5" id="team">
+	<div class="container py-lg-4 py-sm-3">
+        <h3 class="title-big text-center mb-md-5 mb-4">Our Team</h3>
+		<div class="row inner-sec-w3layouts-w3pvt-lauinfo">
+			<div class="col-md-3 col-6">
+				<div class="team-grids text-center">
+					<img src="assets/images/team1.jpg" class="img-fluid" alt=""/>
+					<div class="team-info">
+						<div class="caption">
+							<h4>John Doe</h4>
+							<h6>Founder and CEO</h6>
+						</div>
+					</div>
+				</div>
+				<div class="social-icons-section text-center">
+					<a class="fac" href="#url">
+						<i class="fa fa-facebook"></i>
+					</a>
+					<a class="twitter" href="#url">
+						<i class="fa fa-twitter"></i>
+					</a>
+				</div>
+			</div>
+			<div class="col-md-3 col-6">
+				<div class="team-grids text-center">
+					<img src="assets/images/team2.jpg" class="img-fluid" alt=""/>
+					<div class="team-info">
+						<div class="caption">
+							<h4>Alexander</h4>
+							<h6>Partner & co-founder</h6>
+						</div>
+					</div>
+				</div>
+				<div class="social-icons-section text-center">
+					<a class="fac" href="#url">
+						<i class="fa fa-facebook"></i>
+					</a>
+					<a class="twitter" href="#url">
+						<i class="fa fa-twitter"></i>
+					</a>
+				</div>
+			</div>
+			<div class="col-md-3 col-6 mt-md-0 mt-4">
+				<div class="team-grids text-center">
+					<img src="assets/images/team3.jpg" class="img-fluid" alt=""/>
+					<div class="team-info">
+						<div class="caption">
+							<h4>Martin ker </h4>
+							<h6>Chairmen</h6>
+						</div>
+					</div>
+				</div>
+				<div class="social-icons-section text-center">
+					<a class="fac" href="#url">
+						<i class="fa fa-facebook"></i>
+					</a>
+					<a class="twitter" href="#url">
+						<i class="fa fa-twitter"></i>
+					</a>
+				</div>
+			</div>
+			<div class="col-md-3 col-6 mt-md-0 mt-4">
+				<div class="team-grids text-center">
+					<img src="assets/images/team4.jpg" class="img-fluid" alt=""/>
+					<div class="team-info">
+						<div class="caption">
+							<h4>Elizabeth </h4>
+							<h6>Manager</h6>
+						</div>
+					</div>
+				</div>
+				<div class="social-icons-section text-center">
+					<a class="fac" href="#url">
+						<i class="fa fa-facebook"></i>
+					</a>
+					<a class="twitter" href="#url">
+						<i class="fa fa-twitter"></i>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section> */}
+<section class="w3l-stats">
+    <div class="main-w3 py-5">
+        <div class="container py-lg-3">
+            <div class="row align-items-center">
+                <div class="col-lg-6 title">
+                    <h3 class="title-big">We have a great funfact! Relax & Enjoy your Holiday</h3>
+                    {/* <p class="mt-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo, soluta impedit.
+                        Consequatur,
+                        quasi mollitia? Adipisci exercitationem amet dolore expedita repudiandae voluptatem impedit ipsa
+                        vero
+                        repellendus? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio molestiae ex aut
+                        possimus atque explicabo nostrum recusandae ab quisquam at!</p> */}
+                </div>
+                <div class="col-lg-6 main-cont-wthree-fea text-center">
+                    <div class="row">
+                        <div class="col-6 pr-2 mt-4">
+                            <div class="grids-speci1">
+                                <span class="fa fa-users"></span>
+                                <h3 class="title-spe">1200+</h3>
+                                <p>Happy Clients</p>
+                            </div>
+                        </div>
+                        <div class="col-6 pl-2 mt-4">
+                            <div class="grids-speci1">
+                                <span class="fa fa-camera"></span>
+                                <h3 class="title-spe">18k+</h3>
+                                <p>Luxury Rooms</p>
+                            </div>
+                        </div>
+                        <div class="col-6 pr-2 mt-4">
+                            <div class="grids-speci1">
+                                <span class="fa fa-briefcase"></span>
+                                <h3 class="title-spe">158</h3>
+                                <p>Employers</p>
+                            </div>
+                        </div>
+                        <div class="col-6 pl-2 mt-4">
+                            <div class="grids-speci1">
+                                <span class="fa fa-comments"></span>
+                                <h3 class="title-spe">879+</h3>
+                                <p>Services</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+<section class="w3l-testimonials">
+    <div class="testimonials py-5">
+        <div class="container text-center py-lg-3">
+            <div class="heading text-center mx-auto">
+                {/* <h3 class="title-big text-center">Our Testimonials</h3> */}
+            </div>
+            <div class="row">
+                <div class="col-lg-10 mx-auto">
+                    <div class="owl-testimonial owl-carousel owl-theme">
+                        <div class="item">
+                            <div class="slider-info mt-lg-4 mt-3">
+                                <div class="img-circle">
+                                    <img src="assets/images/team1.jpg" class="img-fluid rounded" alt="client image"/>
+                                </div>
+                                {/* <div class="message">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea sit id
+                                    accusantium
+                                    officia quod quasi necessitatibus perspiciatis Harum error provident
+                                    quibusdam tenetur.</div>
+                                <div class="name">- Johnson</div> */}
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="slider-info mt-lg-4 mt-3">
+                                <div class="img-circle">
+                                    <img src="assets/images/team2.jpg" class="img-fluid rounded" alt="client image"/>
+                                </div>
+                                {/* <div class="message">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea sit id
+                                    accusantium
+                                    officia quod quasi necessitatibus perspiciatis Harum error provident
+                                    quibusdam tenetur.</div>
+                                <div class="name">- Alexander</div> */}
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="slider-info mt-lg-4 mt-3">
+                                <div class="img-circle">
+                                    <img src="assets/images/team3.jpg" class="img-fluid rounded" alt="client image"/>
+                                </div>
+                                {/* <div class="message">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea sit id
+                                    accusantium
+                                    officia quod quasi necessitatibus perspiciatis Harum error provident
+                                    quibusdam tenetur.</div>
+                                <div class="name">- Smith roy</div> */}
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="slider-info mt-lg-4 mt-3">
+                                <div class="img-circle">
+                                    <img src="assets/images/team4.jpg" class="img-fluid rounded" alt="client image"/>
+                                </div>
+                                {/* <div class="message">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea sit id
+                                    accusantium
+                                    officia quod quasi necessitatibus perspiciatis Harum error provident
+                                    quibusdam tenetur.</div>
+                                <div class="name">- Elizabeth</div> */}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<section class="w3l-footer-29-main">
+    <div class="footer-29 py-5">
+      <div class="container py-lg-4">
+        <div class="row footer-top-29">
+          <div class="col-lg-3 col-md-6 col-sm-8 footer-list-29 footer-1">
+            <h6 class="footer-title-29">Contact Us</h6>
+            <ul>
+              <li>
+                <p><span class="fa fa-map-marker"></span>  1354, The Alameda, San Jose
+                  </p>
+              </li>
+              <li><a href=""><span class="fa fa-phone"></span> +(21)-255-999-8888</a></li>
+              <li><a href="" class="mail"><span class="fa fa-envelope-open-o"></span>
+                  hotels@mail.com</a></li>
+            </ul>
+          </div>
+          {/* <div class="col-lg-2 col-md-6 col-sm-4 footer-list-29 footer-2 mt-sm-0 mt-5">
+  
+            <ul>
+              <h6 class="footer-title-29">Useful Links</h6>
+              <li><a href="index.html">Home</a></li>
+              <li><a href="about.html">About hotels</a></li>
+              <li><a href="#blogposts"> Blog posts</a></li>
+              <li><a href="contact.html">Contact us</a></li>
+            </ul>
+          </div> */}
+          {/* <div class="col-lg-3 col-md-6 col-sm-5 footer-list-29 footer-3 mt-lg-0 mt-5">
+              <h6 class="footer-title-29">Latest from blog</h6>
+              <div class="footer-post mb-4">
+                <a href="#url">Work Passionately</a>
+                <p class="small"><span class="fa fa-clock-o"></span> March 9, 2020</p>
+              </div>
+              <div class="footer-post">
+                <a href="#url">Work Passionately without any hesitation</a>
+                <p class="small"><span class="fa fa-clock-o"></span> March 9, 2020</p>
+              </div>
+              
+          </div> */}
+          <div class="col-lg-4 col-md-6 col-sm-7 footer-list-29 footer-4 mt-lg-0 mt-5">
+            <h6 class="footer-title-29">Newsletter </h6>
+            <p>Enter your email and receive the latest news from us.
+             We'll never share your email address</p>
+  
+            <form action="" class="subscribe" method="post">
+              <input type="email" name="email" placeholder="Your Email Address" required=""/>
+              <button><span class="fa fa-envelope-o"></span></button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  
+  <section class="w3l-footer-29-main w3l-copyright">
+    <div class="container">
+      <div class="row bottom-copies">
+        <p class="col-lg-8 copy-footer-29">© 2020 Hotels. All rights reserved | Designed by <a
+            href="">Team BitByBit</a></p>
+  
+        <div class="col-lg-4 main-social-footer-29">
+          <a href="#facebook" class="facebook"><span class="fa fa-facebook"></span></a>
+          <a href="#twitter" class="twitter"><span class="fa fa-twitter"></span></a>
+          <a href="#instagram" class="instagram"><span class="fa fa-instagram"></span></a>
+          <a href="#linkedin" class="linkedin"><span class="fa fa-linkedin"></span></a>
+        </div>
+  
+      </div>
+    </div>
+  
+   <button onclick="topFunction()" id="movetop" title="Go to top">
+    &#10548;
+  </button>
+ 
+  </section>
+
+  
+  </>
+    
+  
+}
+
+export default About
