75d2143e094df3b182b0044a829829b2f17ea902
apparajuakhil <94913578+apparajuakhil@users.noreply.github.com>
2022-04-10 20:01:33 -0700

    added admin apis

---
+++ HotelBookingApplication/backend/Rest/apis/admin.restApi.js
@@ -0,0 +1,55 @@
+const getUsers = async (req, res) => {
+  try {
+      let userInstance = ModelFactory.getUserInstance();
+      let usersList = await userInstance.find();
+      res.status(200).json(usersList);
+  } catch (err) {
+      console.log("err ===>", err);
+      res.status(400).json({ msg: "Error in fetching users data" });
+      return;
+  }
+};
+
+const getUsersById = async (req, res) => {
+  try {
+      let userInstance = ModelFactory.getUserInstance();
+      let user = await userInstance.findOne({_id: req.body.id});
+      res.status(200).json(user);
+  } catch (err) {
+      console.log("err ===>", err);
+      res.status(400).json({ msg: "Error in fetching users data" });
+      return;
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try{
+      let userInstance = ModelFactory.getUserInstance();
+      let user = await userInstance.findOneAndDelete({_id: req.params.userId});
+      res.status(200).json(user);
+  } catch (err) {
+      console.log("err ===>", err);
+      res.status(400).json({ msg: "Error in deleting user data" });
+      return;
+  }
+};
+
+let endpoints = {
+  "/users/": [{
+      method: "GET",
+      callbacks: [getUsers]
+  }],
+  "/users/:userId": [{
+      method: "DELETE",
+      callbacks: [deleteUser]
+  },
+  {
+    method: "GET",
+    callbacks: [getUsersById]
+},
+  ]
+};
+
+export {
+  endpoints
+};
